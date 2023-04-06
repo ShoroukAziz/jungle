9430ea5c83e01b157ebb87b9d54f7059271b3102
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-04-06 06:36:31 -0600

    ➕ login , logout and sign up routes

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,14 @@ Rails.application.routes.draw do
     resources :categories, only: [:create, :new, :index]
   end
 
+  # these routes are for showing users a login form, logging them in, and logging them out.
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  get "/logout" => "sessions#destroy"
+  # these routes are for showing users a signup form and creating a new user
+  get "/signup" => "users#new"
+  post "/users" => "users#create"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
