582bb7898f40bcfe6e015ee7b8915e62782918b1
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-04-06 03:39:45 -0600

    ➕ Admin dashboard stats

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,10 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with :name => ENV["username"], :password => ENV["password"]
 
   def show
+    @products_count = Product.count
+    @categories_count = Category.count
+    @items_in_stock = Product.sum("quantity")
+    @orders_count = Order.count
+    @orders_value = Order.sum("total_cents") / 100
   end
 end
