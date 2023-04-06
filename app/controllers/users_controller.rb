e3a829d698a0691645064a08338bd26189738205
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-04-06 07:34:18 -0600

    🛠 render the form again if errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,15 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
       redirect_to root_path
     else
-      redirect_to signup_path
+      render :new
     end
   end
 
