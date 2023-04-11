cfa8dab658728bff8334a9a2b8643f7f938ca0c7
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-04-11 03:55:24 -0600

    ➕ test add to cart button

---
+++ cypress/integration/add_to_cart.spec.js
@@ -0,0 +1,11 @@
+describe('product details', () => {
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:3000/');
+  });
+
+  it('can add items to cart', () => {
+    cy.contains('My Cart (0)');
+    cy.get('button').contains('Add').first().click();
+    cy.contains('My Cart (1)');
+  });
+});
